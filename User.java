13970eb38908c4868906a4f90aaa176e4d2cd279
xujianning <806529813@qq.com>
2016-12-09 23:55:23 +0800

    用户注册和登陆

---
+++ User.java
@@ -0,0 +1,35 @@
+package com.xujianning;
+import java.io.*;
+public class User {
+	String name;
+	String code;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	User(String name,String code){
+		this.name=name;
+		this.code=code;
+	}
+	void creat() throws IOException{
+		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+		//等待输入用户名
+		name=bf.readLine();
+		//等待输入密码
+		code=bf.readLine();
+		
+	}
+	void load() throws IOException{
+		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+		String n,c;
+		//提示输入用户名
+		n=bf.readLine();
+		//提示输入密码
+		c=bf.readLine();
+		if(n.equals(name)&c.equals(code)){
+			//登陆成功
+		}
+	}
+
+}
