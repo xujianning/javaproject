8cec3810bc2770c2b4681f32d747fa14f5614d1c
xujianning <806529813@qq.com>
2016-12-27 22:12:21 +0800

    Add files via upload

--- DVDrent.java
+++ DVDrent.java
@@ -1,18 +1,19 @@
 package com.xujianning;
 import java.io.*;
 import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
-import com.xujianning.DVD;
-import com.xujianning.User;
+
 public class DVDrent {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
+		menu();
 	}
-	void menu()
+	static void menu()
 	{
 		JFrame jf=new JFrame("请选择功能：");
-		
+		JPanel jp=new JPanel();
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		jf.setBounds(600, 600, 400, 200);
@@ -20,34 +21,99 @@ public class DVDrent {
 		JButton button1=new JButton("DVD租出");
 		JButton button2=new JButton("DVD还回");
 		JButton button3=new JButton("DVD入库");
-		jf.add(button1);
-		jf.add(button2);
-		jf.add(button3);
+		Mouselistener1 m1=new Mouselistener1();
+		button1.addActionListener(m1);
+		Mouselistener2 m2=new Mouselistener2();
+		button2.addActionListener(m2);
+		Mouselistener3 m3=new Mouselistener3();
+		button3.addActionListener(m3);
+		jf.add(jp);
+		jp.add(button3);
+		jp.add(button1);
+		jp.add(button2);
+		
 	
 		
 	}
-	static void stock() throws IOException{
+	void stock() throws IOException{
 		//提示输入新进DVD的名称，编码，购买花费，预定租金和当前库存
-		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
 		JFrame jf=new JFrame("DVD入库");
 		JPanel jp=new JPanel();
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setBounds(600, 600, 400, 200);
+		jf.setVisible(true);
 		JTextArea jt1=new JTextArea();
 		JTextField field=new JTextField();
-		String name;
-		int code,cost,price,num;
-		name=bf.readLine();
-		code=bf.read();
-		cost=bf.read();
-		price=bf.read();
-		num=bf.read();
+		jf.add(jp);
 }
-	static void rent(){
+	void rent(){
 		
+		JFrame jf=new JFrame("DVD租出");
+		JPanel jp=new JPanel();
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setBounds(600, 600, 400, 200);
+		jf.setVisible(true);
+		JButton button=new JButton("输入完成");
+		Mouselistener4 m1=new Mouselistener4();
+		button.addActionListener(m1);
+		jf.add(jp);
+		jp.add(button);
+		//System.out.println("rent");
 	}
-	static void restore(){
-		
+	void restore(){
+		JFrame jf=new JFrame("DVD还回");
+		JPanel jp=new JPanel();
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setBounds(600, 600, 400, 200);
+		jf.setVisible(true);
+		JButton button1=new JButton("输入完成");
+		Mouselistener1 m1=new Mouselistener1();
+		button1.addActionListener(m1);
+		jf.add(jp);
+		jp.add(button1);
+		//System.out.println("restore");
 	}
 
 }
+class Mouselistener1 implements ActionListener{
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		new DVDrent().rent();
+	}
+}
+class Mouselistener2 implements ActionListener{
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		new DVDrent().restore();
+	}
+}
+class Mouselistener3 implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		try {
+			new DVDrent().stock();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+}
+class Mouselistener4 implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		try {
+			
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+}
